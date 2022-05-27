291a5254bcce20f030b989275eee9674f48b4de2
marialauradinis <106067080+marialauradinis@users.noreply.github.com>
2022-05-27 19:33:43 +0100

    included open-source line

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function displayWeather(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = Math.round(response.data.main.temp);
